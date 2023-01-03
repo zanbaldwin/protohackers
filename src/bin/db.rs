7528e121a8c2322d63cbc4797d9ea03ebf8ae9ad
Zan Baldwin <hello@zanbaldwin.com>
2023-01-03 18:44:55 +0100

    Protohackers: Problem 4 (Unusual Database Program)

---
+++ src/bin/db.rs
@@ -0,0 +1,76 @@
+use protozackers::{server};
+use std::collections::HashMap;
+
+const BUFFER_SIZE: usize = 1_000;
+const VERSION_KEY: &[u8] = b"version";
+const VERSION_STRING: &[u8] = b"Zan's Key-Value Store 0.1.0";
+// When testing with netcat, strip newlines that are added to the ends of packets.
+const SHOULD_HANDLE_NEWLINES: bool = false;
+
+struct Database {
+    items: HashMap<Vec<u8>, Vec<u8>>,
+}
+impl Database {
+    fn new() -> Self {
+        Self { items: HashMap::new() }
+    }
+    fn insert(&mut self, key: Vec<u8>, value: Vec<u8>) {
+        if key.as_slice() == VERSION_KEY {
+            return;
+        }
+        self.items.insert(key, value);
+    }
+    fn query(&self, key: &[u8]) -> Option<&[u8]> {
+        if key == VERSION_KEY {
+            return Some(VERSION_STRING);
+        }
+        match self.items.get(key) {
+            Some(value) => Some(value.as_slice()),
+            None => None,
+        }
+    }
+}
+
+fn main() {
+    let socket = server::get_udp_listener(None);
+    let mut database = Database::new();
+
+    loop {
+        let mut buffer = [0u8; BUFFER_SIZE];
+        match socket.recv_from(&mut buffer) {
+            Ok((length, source)) => {
+                let mut request: Vec<u8> = vec![];
+                request.extend_from_slice(&buffer[..length]);
+
+                if SHOULD_HANDLE_NEWLINES {
+                    if let Some(&b'\n') = request.last() {
+                        request.pop();
+                    }
+                }
+
+                if let Some(position) = request.iter().position(|&byte| byte == b'=') {
+                    let mut key: Vec<u8> = vec![];
+                    key.extend_from_slice(request.drain(..position + 1).as_slice());
+                    // Remove the assignment operator (=).
+                    key.pop();
+                    database.insert(key, request);
+                } else {
+                    match database.query(&request) {
+                        Some(value) => {
+                            let mut response: Vec<u8> = vec![];
+                            response.extend_from_slice(&request);
+                            response.push(b'=');
+                            response.extend_from_slice(value);
+                            if SHOULD_HANDLE_NEWLINES {
+                                response.push(b'\n');
+                            }
+                            let _ = socket.send_to(&response, source);
+                        },
+                        None => (),
+                    };
+                }
+            },
+            Err(_) => continue,
+        };
+    }
+}
